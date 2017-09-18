9b156531ab40d9505c28b7f6bb47a7fb90e9915e
willjfish <williamjacksonfisher@gmail.com>
2017-09-18 08:31:01 -0500

    diagonal fixs, documentation, toggles, and created auto classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoLibrary_v1.java
@@ -0,0 +1,24 @@
+/*
+AutoMain_v1
+9/18/2017
+6210 Software
+- William Fisher
+- Rohit Chawla
+- Nihal Kyasa
+
+Holds methods to be used for Autonomous programs in FTC's Relic Recovery Competition.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class AutoLibrary_v1 {
+
+    public void examplemetod()
+    {
+
+    }
+}
