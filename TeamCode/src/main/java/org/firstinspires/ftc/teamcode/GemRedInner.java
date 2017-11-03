110cbf7d5f1b56f4649ffb3c12df8df98cbf8602
rohitchawla28 <31925602+rohitchawla28@users.noreply.github.com>
2017-11-03 09:18:07 -0500

    changes to red inner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GemRedInner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GemRedInner.java
@@ -1,10 +1,18 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+
 /**
  * Created by Rohit on 11/3/17.
  */
+@Autonomous(name="GemRedInner", group="Auto")
+public class GemRedInner extends AutoLibrary_v2{
 
-public class GemRedInner {
+    private ElapsedTime runtime = new ElapsedTime();
+    private RelicRecoveryVuMark
 
-    
+
 }
