913819d4c6a963d3b107dd7cd3202c7ad72b1278
rohitchawla28 <31925602+rohitchawla28@users.noreply.github.com>
2017-11-13 23:49:49 -0600

    finished line to line and finished vuforia practice auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/P_Auto_LineToLine_Rohit.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/P_Auto_LineToLine_Rohit.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
  * Created by Rohit on 11/8/17.
  */
 
-public class P_Auto_parktopark extends Practice_Auto_Library_Rohit{
+public class P_Auto_LineToLine_Rohit extends Practice_Auto_Library_Rohit{
 
     @Override
     public void runOpMode() throws InterruptedException
@@ -17,6 +17,10 @@ public class P_Auto_parktopark extends Practice_Auto_Library_Rohit{
         cs_move_to_line(0.5, angle, 3);
         move_encoder(0.5, 10, 2);
         cs_move_to_line(0.5, angle, 3);
+        if (getBlueGem(10))
+        {
+            gyro_turn(-90, 3);
+        }
         stop_motors();
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice_Auto_Library_Rohit.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice_Auto_Library_Rohit.java
@@ -39,18 +39,26 @@ public abstract class Practice_Auto_Library_Rohit extends LinearOpMode {
     Orientation angles;
     Acceleration gravity;
 
+    public static final String TAG = "Vuforia VuMark Sample";
+    OpenGLMatrix lastLocation = null;
+    VuforiaLocalizer vuforia;
+    VuforiaTrackable relicTemplate;
+    VuforiaTrackables relicTrackables;
+
     public DcMotor bldrive;
     public DcMotor brdrive;
     public DcMotor fldrive;
     public DcMotor frdrive;
+    public DcMotor output;
     public ColorSensor cs;
     public ColorSensor gs;
 
-    public void initialize() {
+    public void initialize() throws InterruptedException {
         frdrive = hardwareMap.get(DcMotor.class, "a");
         fldrive = hardwareMap.get(DcMotor.class, "b");
         brdrive = hardwareMap.get(DcMotor.class, "c");
         bldrive = hardwareMap.get(DcMotor.class, "d");
+        output = hardwareMap.get(DcMotor.class, "out");
         cs = hardwareMap.get(ColorSensor.class, "cs");
         gs = hardwareMap.get(ColorSensor.class, "gs");
         fldrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -59,7 +67,6 @@ public abstract class Practice_Auto_Library_Rohit extends LinearOpMode {
         frdrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         brdrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         bldrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        
 
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
         parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
@@ -74,50 +81,73 @@ public abstract class Practice_Auto_Library_Rohit extends LinearOpMode {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
+        vision_init();
+
         waitForStart();
     }
-    
+
+    public void vision_init() throws InterruptedException {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+        parameters.vuforiaLicenseKey = "ARUX4tP/////AAAAGXY2Dg+/sUl6gWdYntfHvN8GT9v/tqySPvCz3Nt2dTXFWQC7TJriGnCTY/vvHRRUFiSSI11yfUxGSTkNzXbHM0zBmGf3WiW6+kZsArc76UHXbUG1fHmPyIAljbqRBiNz8Kki/PlrJCwpNwmcZKNu8wvnYzGZ5phfZHXE6yyr2HvuEyX6IEYUvrvDtMImiHWHSbjK5wbgDyMinQU/FsVmDy0S1OHL+xVDk6yhjBsPBO2bsVMTKA3GRZAo+Qxjqd9nh95+jPt1EbE11VgPHzr/Zm8bKrr+gz24uxfsTgXU3sc6YLgdcegkRd6dxM5gvsu4xisSks+gkLismFPmNASP0JpDkom80KZ9MmEcbl7GnLO+";
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.FRONT;
+        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+        VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
+        VuforiaTrackable relicTemplate = relicTrackables.get(0);
+        relicTemplate.setName("relicVuMarkTemplate"); // can help in debugging; otherwise not necessary
+    }
+
+
     //==================================================== BASIC MOVEMENT METHODS ===================================================
 
-    public void move_yaxis_basic(double power) {
+    public void move_yaxis_basic(double power)
+    {
         frdrive.setPower(power);
         brdrive.setPower(power);
         fldrive.setPower(-power);
         bldrive.setPower(-power);
     }
 
-    public void turn_basic(double power) {
+    public void move_xaxis_basic(double power)
+    {
+        frdrive.setPower(power);
+        brdrive.setPower(-power);
+        fldrive.setPower(power);
+        bldrive.setPower(-power);
+    }
+
+    public void turn_basic(double power)
+    {
         frdrive.setPower(-power);
         brdrive.setPower(-power);
         fldrive.setPower(-power);
         brdrive.setPower(-power);
     }
 
-    public void stop_motors() {
+    public void stop_motors()
+    {
         frdrive.setPower(0);
         brdrive.setPower(0);
         fldrive.setPower(0);
         brdrive.setPower(0);
     }
-    
+
     //================================================ Gyro Turn ===================================================================
-    
+
     public void gyro_turn(double target_angle, double threshold) {
         double currentAngle = getAngle();
-        while (Math.abs(target_angle - getAngle()) > threshold)
-        {
+        while (Math.abs(target_angle - getAngle()) > threshold) {
             if (target_angle - currentAngle > 0) {
-            turn_basic(1);
-            }
-            else {
-            turn_basic(-1);
+                turn_basic(1);
+            } else {
+                turn_basic(-1);
             }
         }
         stop_motors();
     }
 
     //================================================= Gyro Correct ==================================================================
-    
+
     public void move_gyro_correct(double power, double targetAngle, double threshold)
     {
         frdrive.setPower(power * getRcorrect(targetAngle, threshold));
@@ -126,7 +156,8 @@ public abstract class Practice_Auto_Library_Rohit extends LinearOpMode {
         bldrive.setPower(-power * getLcorrect(targetAngle, threshold));
     }
 
-    public double getRcorrect(double targetAngle, double threshold) {
+    public double getRcorrect(double targetAngle, double threshold)
+    {
         if (targetAngle - getAngle() > threshold) {
             return 1 + (Math.abs(targetAngle - getAngle())) / 90;
         } else if (targetAngle - getAngle() < threshold) {
@@ -135,7 +166,8 @@ public abstract class Practice_Auto_Library_Rohit extends LinearOpMode {
         return 1;
     }
 
-    public double getLcorrect(double targetAngle, double threshold) {
+    public double getLcorrect(double targetAngle, double threshold)
+    {
         if (targetAngle - getAngle() < threshold) {
             return 1 + (Math.abs(targetAngle - getAngle())) / 90;
         } else if (targetAngle - getAngle() > threshold) {
@@ -145,48 +177,79 @@ public abstract class Practice_Auto_Library_Rohit extends LinearOpMode {
     }
 
     //================================================= Get Encoder =====================================================================
-    
+
     public double getEncoderAvg()
     {
-        return (frdrive.getCurrentPosition() + fldrive.getCurrentPosition() + brdrive.getCurrentPosition() + bldrive.getCurrentPosition())/4;
+        return (frdrive.getCurrentPosition() + fldrive.getCurrentPosition() + brdrive.getCurrentPosition() + bldrive.getCurrentPosition()) / 4;
     }
-    
+
     //=============================================== Get Angle ============================================================================
-    
+
     public double getAngle()
     {
         angles = gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
         return angles.firstAngle;
     }
-    
+
     //=============================================Color Sensor Move to Line=============================================================
-    
+
     public void cs_move_to_line(double power, double targetAngle, double threshold)
     {
         double encoder_Start = getEncoderAvg();
-        while (opModeIsActive() && cs.alpha() < 100 && (Math.abs(getEncoderAvg() - encoder_Start)) < 100);
+        while (opModeIsActive() && cs.alpha() < 100 && (Math.abs(getEncoderAvg() - encoder_Start)) < 100)
+            ;
         {
             move_gyro_correct(power, targetAngle, threshold);
 
         }
-            stop_motors();
+        stop_motors();
     }
 
-    //============================================= Move Encoder ======================================================================
+    //============================================= Move Encoder Y Axis ======================================================================
 
-    public void move_encoder(double power, double distance, double threshold)
+    public void move_encoder_yaxis(double power, double distance)
     {
-        while (Math.abs(distance - getEncoderAvg()) > threshold)
+        double start = getEncoderAvg();
+        while (Math.abs(getEncoderAvg() - start) < distance)
         {
             move_yaxis_basic(power);
         }
         stop_motors();
     }
 
+    //===================================================== Move Encoder X Axis =============================================================
+
+    public void move_encoder_xaxis(double power, double distance)
+    {
+        double start = getEncoderAvg();
+        while (Math.abs(distance - getEncoderAvg()) < distance)
+        {
+            move_xaxis_basic(power);
+        }
+        stop_motors();
+    }
+
     //============================================ Get Gem ===========================================================================
 
-    public void getGem(double power, )
-    {
-        gs.blue();
+    public boolean getBlueGem(double threshold) {
+        if (gs.blue() > threshold) {
+            return true;
+        }
+        return false;
     }
+
+    public boolean getBlueGem_Short(double threshold) {
+        return (gs.blue() > threshold);
+    }
+
+    //Uses vuforia to detect the mark
+    //Note - RelicRecoveryVuMark is a Class unique to vuforia code - think of it as a data type like boolean or double
+    //can return RelicRecoveryVuMark.UNKNOWN, RelicRecoveryVuMark.RIGHT, R-k.LEFT, or R-k.CENTER
+    public RelicRecoveryVuMark getSymbol() {
+        relicTrackables.activate();
+        RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
+        relicTrackables.deactivate();
+        return vuMark;
+    }
+
 }
